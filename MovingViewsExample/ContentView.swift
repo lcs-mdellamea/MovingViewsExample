3178c2b415b9a602b40f4bf48d1c36042b66cead
Russell Gordon <github@russellgordon.ca>
2022-05-08 09:31:43 -0400

    Apple falls down the screen, registers taps.

--- MovingViewsExample/ContentView.swift
+++ MovingViewsExample/ContentView.swift
@@ -10,12 +10,29 @@ import SwiftUI
 struct ContentView: View {
     
     // MARK: Stored properties
+    let appleTimer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
+    @State var appleOffset = -200.0
     
     // MARK: Computed properties
     var body: some View {
+        
         Image("apple")
             .resizable()
             .frame(width: 75, height: 75)
+            .offset(y: appleOffset)
+            .onReceive(appleTimer) { input in
+                
+                withAnimation(
+                    Animation
+                        .linear(duration: 1.0)
+                ) {
+                    appleOffset += 15.0
+                }
+                
+            }
+            .onTapGesture {
+                print("Apple was tapped.")
+            }
     }
 }
 
